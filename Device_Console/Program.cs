ce02119f6e6e5c58ad23097236569d8e8bfcc6d7
Gong-Code <gong.moonphruk@gmail.com>
2022-09-05 17:48:22 +0200

    first commit

---
+++ Device_Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System.Net.Http.Json;
+using System.Text;
+
+
+namespace Device_Console
+{
+    class Program
+    {
+        private static DeviceClient _deviceClient;
+        private static string _deviceId = "b48de464-3bc6-4121-b43c-2ecc568241f7";
+        public static async Task Main()
+        {
+            await InitializeAsync(_deviceClient, _deviceId);
+        }
+
+        private static async Task InitializeAsync(DeviceClient deviceClient, string deviceId)
+        {
+            using var client = new HttpClient();
+
+            var result = await client.GetAsync(
+                $"https://function-app-api.azurewebsites.net/api/GetDevice?code=IDPaGA4bbYvY1liOlosh7LNStAtA_clgmyv6cnVNhss_AzFuUbjhJA==&deviceId={deviceId}");
+            var connectionString = await result.Content.ReadAsStringAsync();
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                result = await client.PostAsJsonAsync(
+                    "https://function-app-api.azurewebsites.net/api/CreateDevice?code=AsPPnkW3IOvObx73iy0NhyN7mBitR1yn47jr439wGXjqAzFurh2sMw==", new { deviceId = deviceId });
+                connectionString = await result.Content.ReadAsStringAsync();
+            }
+
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+            Console.WriteLine($"Device Connection: {connectionString}");
+            Console.WriteLine("Device Connected");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ WebApi/Sevices/DeviceService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.Devices;
+
+namespace WebApi.Sevices
+{
+    public interface IDeviceService
+    {
+        public Task<string> CreateDeviceAsync(string deviceId);
+        public Task<Device> GetDeviceAsync(string deviceId);
+    }
+
+    public class DeviceService : IDeviceService
+    {
+        private readonly RegistryManager _registryManager;
+        private readonly IConfiguration _configuration;
+
+        public DeviceService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _registryManager = RegistryManager.CreateFromConnectionString(_configuration.GetConnectionString("IotHub"));
+        }
+
+        public async Task<string> CreateDeviceAsync(string deviceId)
+        {
+            var device = await _registryManager.AddDeviceAsync(new Device(deviceId));
+            
+            return $"HostName=systemDev-IotHub.azure-devices.net;DeviceId={device.Id};SharedAccessKey{device.Authentication.SymmetricKey.PrimaryKey}";
+        }
+
+        public Task<Device> GetDeviceAsync(string deviceId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
